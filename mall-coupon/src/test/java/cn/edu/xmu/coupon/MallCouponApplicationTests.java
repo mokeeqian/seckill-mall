54d7e03f99a843ffd6f9a17ab69257fa40141d32
mokeeqian <mokeeqian@gmail.com>
2022-08-14 18:35:14 +0800

    Initial commit

---
+++ mall-coupon/src/test/java/cn/edu/xmu/coupon/MallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.edu.xmu.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ mall-ware/src/test/java/cn/edu/xmu/ware/MallWareApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.edu.xmu.ware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallWareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
