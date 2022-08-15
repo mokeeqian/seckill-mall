2f1b0d483da6b4a6aaf9fd7ccda48256c4e1f668
mokeeqian <mokeeqian@gmail.com>
2022-08-15 15:37:23 +0800

    整活coupon

--- mall-coupon/src/main/java/cn/edu/xmu/coupon/MallCouponApplication.java
+++ mall-coupon/src/main/java/cn/edu/xmu/coupon/MallCouponApplication.java
@@ -2,7 +2,9 @@ package cn.edu.xmu.coupon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+//@ComponentScan("cn.edu.xmu.coupon.dao")
 @SpringBootApplication
 public class MallCouponApplication {
 
