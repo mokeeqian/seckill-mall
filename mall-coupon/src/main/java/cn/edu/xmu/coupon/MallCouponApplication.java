d05a012947ce449b519ae56db789eba2a5d7329b
mokeeqian <mokeeqian@gmail.com>
2022-08-15 17:04:58 +0800

    整活 nacos via coupon & member

--- mall-coupon/src/main/java/cn/edu/xmu/coupon/MallCouponApplication.java
+++ mall-coupon/src/main/java/cn/edu/xmu/coupon/MallCouponApplication.java
@@ -2,14 +2,16 @@ package cn.edu.xmu.coupon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
 //@ComponentScan("cn.edu.xmu.coupon.dao")
+// 开启服务发现
+@EnableDiscoveryClient
 @SpringBootApplication
 public class MallCouponApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MallCouponApplication.class, args);
     }
-
 }
